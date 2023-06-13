da3c675392ede3a2941ede5ff6de1997238b3580
EverEmpty <fairel95@gmail.com>
2023-06-13 23:35:27 +0300

    corrected

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -7,7 +7,6 @@ import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
-import java.util.List;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
@@ -20,15 +19,6 @@ public class MainApp {
       userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("UserCar2", 2)));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("UserCar3", 3)));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("UserCar4", 4)));
-
-//      List<User> users = userService.listUsers();
-//      for (User user : users) {
-//         System.out.println("Id = "+user.getId());
-//         System.out.println("First Name = "+user.getFirstName());
-//         System.out.println("Last Name = "+user.getLastName());
-//         System.out.println("Email = "+user.getEmail());
-//         System.out.println();
-//      }
 
       System.out.println();
       System.out.println("User: " + userService.getUserByCar("UserCar1", 1));
