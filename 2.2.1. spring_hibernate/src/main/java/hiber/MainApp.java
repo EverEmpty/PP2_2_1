369d1795b7ee593286268c41daa0f3b15b543e5f
EverEmpty <fairel95@gmail.com>
2023-06-14 22:39:19 +0300

    corrected

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -7,7 +7,7 @@ import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainApp {
-   public static void main(String[] args){
+   public static void main(String[] args) {
       AnnotationConfigApplicationContext context = 
             new AnnotationConfigApplicationContext(AppConfig.class);
 
